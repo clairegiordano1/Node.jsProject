b61067e3c804eff04b077d7e1d8d57e24b584696
Jake <jakejaekwak@gmail.com>
2020-06-19 15:09:02 -0400

    implement ls

--- bash.js
+++ bash.js
@@ -1,4 +1,5 @@
 const pwd = require('./pwd.js');
+const ls = require('./ls.js');
 
 console.log(pwd);
 //Output a prompt
@@ -8,9 +9,13 @@ process.stdout.write('prompt >');
 //The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
 	const cmd = data.toString().trim(); //remove the newline
-	
+
 	if (pwd(cmd)) pwd(cmd)
+	else if (cmd === 'ls')  ls()
 	else process.stdout.write("You typed: " + cmd);
 
 	process.stdout.write("\nprompt > ");
 });
+
+
+
